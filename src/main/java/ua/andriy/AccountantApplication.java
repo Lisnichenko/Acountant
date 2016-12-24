3b654e158e5f5bc8221c9221419ab0d914407211
Lisnichenko <a.lisnichenko@gmail.com>
2016-12-24 13:48:33 +0200

    Cleanup jQuery scripts and old comments

--- src/main/java/ua/andriy/AccountantApplication.java
+++ src/main/java/ua/andriy/AccountantApplication.java
@@ -2,8 +2,6 @@ package ua.andriy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class AccountantApplication {
@@ -11,9 +9,5 @@ public class AccountantApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountantApplication.class, args);
 	}
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-//    }
 
 }
